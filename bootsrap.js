d692e801a0b3bbca43299b32ed50d2738daa2a10
J.A.I. Sampath <161950512+Isuru95sampath@users.noreply.github.com>
2025-03-20 12:05:10 +0530

    complete web site without animation

---
+++ bootsrap.js
@@ -0,0 +1,8 @@
+// Initialize AOS animations
+document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+        duration: 1000, // Animation duration in milliseconds
+        once: true, // Animation occurs only once
+        easing: "ease-in-out", // Smooth transition
+    });
+});
